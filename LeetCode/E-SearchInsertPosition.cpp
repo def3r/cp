49bd12b73eaf01de6880f8f8be9663ba2444f55b
horrifyingHorse <excuseplanation@gmail.com>
2025-07-20 13:11:21 +0530

    Solution 35.Search Insert Position
    https://leetcode.com/submissions/detail/1704496039/

---
+++ LeetCode/E-SearchInsertPosition.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define fast_io()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+
+void __setup() {
+  fast_io();
+
+#ifndef ONLINE_JUDGE
+  freopen("_input.txt", "r", stdin);
+  // freopen("_output.txt", "w", stdout);
+#endif
+}
+
+int searchInsert(vector<int> &nums, int target) {
+  int left, right, mid;
+  left = 0, right = nums.size() - 1;
+  while (left <= right) {
+    mid = (left + right) / 2;
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  if (nums[mid] < target) {
+    mid++;
+  }
+  return mid;
+}
+
+int main() {
+  __setup();
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+  }
+
+  return 0;
+}
