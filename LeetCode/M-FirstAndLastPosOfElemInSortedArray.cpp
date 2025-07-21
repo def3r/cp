b06d6013062fc68a0a47bfae4b58c259f50f32be
horrifyingHorse <excuseplanation@gmail.com>
2025-07-21 21:20:33 +0530

    Solution 34.Find First and Last Position of Element in Sorted Array
    https://leetcode.com/submissions/detail/1706135438/

---
+++ LeetCode/M-FirstAndLastPosOfElemInSortedArray.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define fast_io()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+
+void __setup() {
+  fast_io();
+
+#ifndef ONLINE_JUDGE
+  freopen("_input.txt", "r", stdin);
+  // freopen("_output.txt", "w", stdout);
+#endif
+}
+
+int main() {
+  __setup();
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+  }
+
+  return 0;
+}
